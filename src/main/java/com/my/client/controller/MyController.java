50dc787e27d0e82e62b9f052bf8179c749b444f4
武为江 <by_wwj@163.com>
2018-02-09 11:36:12 +0800

    aa

---
+++ src/main/java/com/my/client/controller/MyController.java
@@ -0,0 +1,22 @@
+package com.my.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Administrator on 2018/2/8.
+ */
+@Controller
+public class MyController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(Model model) {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printIndex(Model model) {
+        return "hello";
+    }
+}
